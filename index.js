d9de2c43255d7e1cb1f33c09a64deae35c98f12e
Taz Jackson <tatjsn@users.noreply.github.com>
2020-05-04 19:41:07 +0100

    Use es6 import

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-goog.module('tatjsn.app');
+import api from 'goog:google3.javascript.template.soy.api_idom';
+import simple from 'goog:soy.examples.simple.incrementaldom';
 
-var api = goog.require('google3.javascript.template.soy.api_idom');
-var simple = goog.require('soy.examples.simple.incrementaldom');
-var renderer = new api.IncrementalDomRenderer();
+const renderer = new api.IncrementalDomRenderer();
 
 function renderPart() {
   simple.helloWorld(renderer);
